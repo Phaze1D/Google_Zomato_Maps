e4cd7499b769cf91b0ff6e3f4bdffdea6220cd98
DavidMBP <steadypathapp@gmail.com>
2017-04-18 22:08:52 -0500

    Created webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var path = require('path');
+
+
+var config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      'src'
+    ],
+    extensions: ['.js']
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+          plugins: ["transform-decorators-legacy"]
+        }
+      }]
+    },{
+      test: /\.sass$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    },{
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: true
+        }
+      }]
+    }]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+  }
+}
+
+module.exports = config
