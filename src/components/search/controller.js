3fa53cc7fccd5778af3a0370bf733e2546f5788a
DavidMBP <steadypathapp@gmail.com>
2017-04-19 14:36:56 -0500

    Added basic search animations

--- src/components/search/controller.js
+++ src/components/search/controller.js
@@ -8,6 +8,30 @@ import { component } from 'utils/decorators'
 })
 class SearchController {
   constructor(params) {
+    this.didSubmit = false;
+    this.showHolder = ko.observable(true);
+    this.showClear = ko.observable(true);
+  }
 
+  handleFocus(data, event){
+    if(event.target.value.length > 0){
+      this.showHolder(false);
+      this.showClear( (event.type === 'focusin' || this.didSubmit)  )
+    }else{
+      this.showHolder(!this.showHolder());
+    }
+  }
+
+  handleSubmit(data){
+    this.showClear(true);
+    this.didSubmit = true;
+  }
+
+  handleReset(data, event){
+    this.didSubmit = false;
+    var input = document.getElementById('search-input');
+    input.value = ''
+    input.blur()
+    this.showHolder(true)
   }
 }
