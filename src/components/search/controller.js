6ddb7e05889662c8b7f811be6d107a7590f80c90
DavidMBP <steadypathapp@gmail.com>
2017-04-18 22:12:11 -0500

    Started the search input

---
+++ src/components/search/controller.js
@@ -0,0 +1,13 @@
+import ko from 'knockout'
+import { component } from 'utils/decorators'
+
+
+@component({
+  name: 'search-input',
+  template: require('./view.html')
+})
+class SearchController {
+  constructor(params) {
+
+  }
+}
