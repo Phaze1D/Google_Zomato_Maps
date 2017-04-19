996c2d5989e82cba530fa8717fbab2fc4360de3c
DavidMBP <steadypathapp@gmail.com>
2017-04-18 22:12:40 -0500

    Created the map view model

---
+++ src/components/google_map/controller.js
@@ -0,0 +1,54 @@
+import ko from 'knockout'
+import GoogleMapsLoader from 'google-maps'
+import { component } from 'utils/decorators'
+
+
+
+
+@component({
+  name: 'google-map',
+  template: require('./view.html')
+})
+class GoogleMapController {
+  constructor(params) {
+    this.showMap = ko.observable(false);
+
+    GoogleMapsLoader.KEY = 'AIzaSyAjN_VCsrqtlCqyozjTV7L8z_JYMeYBKzg';
+    GoogleMapsLoader.LIBRARIES = ['places'];
+    GoogleMapsLoader.load(this.getUserLocation.bind(this))
+  }
+
+  getUserLocation(google){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        this.initMap(google, pos)
+
+      }, () => {
+        this.handleError();
+        this.initMap(google, {lat: -7.1562833, lng: 110.0800594});
+      });
+
+    }else{
+      this.handleError();
+      this.initMap(google, {lat: -7.1562833, lng: 110.0800594});
+    }
+  }
+
+  initMap(google, pos){
+    this.showMap(true);
+    this.map = new google.maps.Map(document.getElementById('google-map'), {
+      center: pos,
+      scrollwheel: false,
+      zoom: 10,
+      disableDefaultUI: true
+    });
+  }
+
+  handleError(){
+
+  }
+}
