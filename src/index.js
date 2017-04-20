3b65e65c23e9b2cf6b6d6c77e0bf711ba23f1d88
DavidMBP <steadypathapp@gmail.com>
2017-04-19 21:24:35 -0500

    Import missing components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import ko from 'knockout'
 import 'components/google_map'
 import 'components/spinner'
 import 'components/search'
+import 'components/fab'
+import 'components/autocomplete'
+import 'components/results'
 import 'index.sass'
 
 
