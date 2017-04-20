34fbf3a14c2c8cf17271c6c5967c7e6c1d2b8a17
DavidMBP <steadypathapp@gmail.com>
2017-04-19 21:22:46 -0500

    Implemented autocomplete component

--- src/components/search/controller.js
+++ src/components/search/controller.js
@@ -1,5 +1,5 @@
-import ko from 'knockout'
-import { component } from 'utils/decorators'
+import ko from 'knockout';
+import { component } from 'utils/decorators';
 
 
 @component({
@@ -11,27 +11,76 @@ class SearchController {
     this.didSubmit = false;
     this.showHolder = ko.observable(true);
     this.showClear = ko.observable(true);
+    this.showAuto = ko.observable(false);
+    this.autoList = ko.observableArray();
+
+    this.onItemCallback = this.onItemCallback.bind(this);
+
+    var shis = JSON.parse(localStorage.getItem('searchHistory'));
+    this.autoList = ko.observableArray(shis);
   }
 
-  handleFocus(data, event){
-    if(event.target.value.length > 0){
-      this.showHolder(false);
-      this.showClear( (event.type === 'focusin' || this.didSubmit)  )
-    }else{
-      this.showHolder(!this.showHolder());
+  addValue(value){
+    this.autoList.unshift(value);
+    if(this.autoList().length > 5){
+      this.autoList.pop();
     }
   }
 
-  handleSubmit(data){
+  convertValue(value){
+    value = value.split(',', 1);
+    value.length == 1 ? value.push('') : value
+    return {icon: 'schedule', main: value[0], sub: value[1]};
+  }
+
+  updateLocalStorage(value){
+    var history =  JSON.parse(localStorage.getItem('searchHistory'));
+    history ? history.unshift(value) : history = [value];
+    if(history.length > 5){
+      history.pop();
+    }
+    localStorage.setItem('searchHistory', JSON.stringify(history));
+  }
+
+  handleFocusIn(data, event){
     this.showClear(true);
+    this.showHolder(false);
+    this.showAuto(this.autoList().length > 0);
+  }
+
+  handleFocusOut(data, event){
+    this.showClear(this.didSubmit);
+    this.showHolder(event.target.value.length == 0);
+    this.showAuto(false);
+  }
+
+  handleOnChange(data, event){
+    this.didSubmit = false;
+  }
+
+  handleSubmit(data, createNew=true){
     this.didSubmit = true;
+    this.showAuto(false);
+    this.showClear(true);
+    var input = document.getElementById('search-input');
+    input.blur();
+    if(createNew){
+      var value = this.convertValue(input.value);
+      this.updateLocalStorage(value);
+      this.addValue(value);
+    }
   }
 
   handleReset(data, event){
     this.didSubmit = false;
     var input = document.getElementById('search-input');
-    input.value = ''
-    input.blur()
-    this.showHolder(true)
+    input.value = '';
+    input.blur();
+    this.showHolder(true);
+  }
+
+  onItemCallback(value){
+    document.getElementById('search-input').value = value;
+    this.handleSubmit(null, false);
   }
 }
