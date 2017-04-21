b8708299f266069c4cc2ad2f8dda80245fe64197
DavidMBP <steadypathapp@gmail.com>
2017-04-20 21:58:58 -0500

    Added missing imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import ko from 'knockout'
-import 'components/google_map'
-import 'components/spinner'
-import 'components/search'
-import 'components/fab'
-import 'components/autocomplete'
-import 'components/results'
-import 'index.sass'
+import ko from 'knockout';
+import 'components/google_map';
+import 'components/spinner';
+import 'components/search';
+import 'components/fab';
+import 'components/autocomplete';
+import 'components/results';
+import 'components/stars';
+import 'components/item';
+import 'index.sass';
 
 
 ko.applyBindings();
