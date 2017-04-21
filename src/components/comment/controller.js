ff8d730d040376099974e67272b37ee171c8dca8
DavidMBP <steadypathapp@gmail.com>
2017-04-21 15:02:44 -0500

    Added the comment section

---
+++ src/components/comment/controller.js
@@ -0,0 +1,14 @@
+import ko from 'knockout';
+import { component } from 'utils/decorators';
+
+
+
+@component({
+  name: 'review-comment',
+  template: require('./view.html')
+})
+class CommentController {
+  constructor(params) {
+    
+  }
+}
