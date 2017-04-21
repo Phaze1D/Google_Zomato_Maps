e1d09f80dd6cc1b88cac7fa0bdd8eab11237e990
DavidMBP <steadypathapp@gmail.com>
2017-04-21 15:03:10 -0500

    Finished the item component

--- src/components/item/controller.js
+++ src/components/item/controller.js
@@ -10,11 +10,27 @@ import { fakeResult } from '../../../fake.js';
   name: 'item',
   template: require('./view.html')
 })
-
 class ItemController {
   constructor(params) {
     this.mainImage = 'http://lh5.googleusercontent.com/-58Hn-jVwMEk/WD9UjLx_4JI/AAAAAAAATqw/P0CXx6b8vrUrXUiyaUPdnEo07pzkFziSwCLIB/w455-h256-k-no/'
-
     this.result = fakeResult();
+    this.over = false
+
+    this.onRequestBack = params.onRequestBack
+  }
+
+  handleScroll(data, event) {
+    if(event.target.scrollTop > event.target.clientHeight * 0.3 && !this.over){
+      document.getElementById('item-search-background').style.opacity = "1"
+      this.over = true
+    }
+    if(event.target.scrollTop < event.target.clientHeight * 0.3 && this.over) {
+      document.getElementById('item-search-background').style.opacity = "0"
+      this.over = false
+    }
+  }
+
+  handleBack(data, event) {
+    this.onRequestBack()
   }
 }
