7e31bf3e679f8502c775a72952223c24821cb63c
DavidMBP <steadypathapp@gmail.com>
2017-04-22 20:07:50 -0500

    Finished google detail search

--- src/components/google_map/controller.js
+++ src/components/google_map/controller.js
@@ -1,7 +1,7 @@
 import ko from 'knockout';
 import { component } from 'utils/decorators';
 import { mapSingleton } from 'utils/map';
-import { searchTextRequest } from 'actions/action'
+import { searchTextRequest, googlePlaceDetailRequest } from 'actions/action';
 
 
 
@@ -14,6 +14,7 @@ class GoogleMapController {
   constructor(params) {
     this.showMap = ko.observable(false);
     this.fetching = ko.observable(false);
+    this.place = ko.observable({});
     this.results = ko.observableArray();
     mapSingleton.load(this.onMapLoadCallback.bind(this));
 
@@ -22,6 +23,7 @@ class GoogleMapController {
     this.onItemCallback = this.onItemCallback.bind(this);
     this.onItemBackCallback = this.onItemBackCallback.bind(this);
     this.onSearchTextCallback = this.onSearchTextCallback.bind(this);
+    this.onGoogleItemCallback = this.onGoogleItemCallback.bind(this);
   }
 
   showResults(show){
@@ -59,9 +61,9 @@ class GoogleMapController {
     this.showItem(false);
   }
 
-  onItemCallback(){
-    this.showResults(false);
-    this.showItem(true);
+  onItemCallback(data){
+    this.fetching(true);
+    googlePlaceDetailRequest({placeId: data.place_id}, this.onGoogleItemCallback)
   }
 
   onItemBackCallback(){
@@ -70,19 +72,29 @@ class GoogleMapController {
   }
 
   onSearchTextCallback(results, status){
-    console.log(results);
-    this.fetching(false)
-    this.showResults(true);
-    this.showItem(false);
     for (var i = 0; i < results.length; i++) {
       this.results.push({
+        place_id: results[i].place_id,
         title: results[i].name,
-        review: results[i].rating,
-        type: results[i].types ? results[i].types[0] : '',
+        rating: results[i].rating,
+        type: results[i].types ? results[i].types[0].replace("_", " ") : '',
         address: results[i].vicinity,
         open: results[i].opening_hours ? results[i].opening_hours.open_now : false,
-        picture: results[i].photos ? results[i].photos[0].getUrl({'maxWidth': 160, 'maxHeight': 160}) : '',
+        picture: results[i].photos ? results[i].photos[0].getUrl({'maxWidth': 160, 'maxHeight': 160}) : 'https://maps.gstatic.com/tactile/omnibox/list-result-no-thumbnail-1x.png',
       })
     }
+    this.fetching(false);
+    this.showResults(true);
+    this.showItem(false);
+  }
+
+  onGoogleItemCallback(place, status){
+    place.type = place.types ? place.types[0].replace("_", " ") : '';
+    place.photo = place.photos ? place.photos[0].getUrl({'maxWidth': 400}) : 'https://maps.gstatic.com/tactile/pane/default_geocode-2x.png';
+    place.open = place.opening_hours ? place.opening_hours.open_now : false
+    this.place(place);
+    this.showResults(false);
+    this.showItem(true);
+    this.fetching(false);
   }
 }
