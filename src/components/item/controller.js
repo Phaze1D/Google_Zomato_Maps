33487c42b5aa5784a911ef7c4b180b93afc829ec
DavidMBP <steadypathapp@gmail.com>
2017-04-22 20:07:31 -0500

    Added google details names

--- src/components/item/controller.js
+++ src/components/item/controller.js
@@ -12,8 +12,7 @@ import { fakeResult } from '../../../fake.js';
 })
 class ItemController {
   constructor(params) {
-    this.mainImage = 'http://lh5.googleusercontent.com/-58Hn-jVwMEk/WD9UjLx_4JI/AAAAAAAATqw/P0CXx6b8vrUrXUiyaUPdnEo07pzkFziSwCLIB/w455-h256-k-no/'
-    this.result = fakeResult();
+    this.place = params.place;
     this.over = false
 
     this.onRequestBack = params.onRequestBack
