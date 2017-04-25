5052909585131eee1d0fd7a51e172ba036f0a25b
DavidMBP <steadypathapp@gmail.com>
2017-04-25 00:50:45 -0500

    Added mouse enter and leaves handlers

--- src/components/results/controller.js
+++ src/components/results/controller.js
@@ -9,13 +9,26 @@ import { fakeResult } from '../../../fake.js'
 })
 class ResultsController {
   constructor(params) {
-    this.onRequestItem = params.onRequestItem
-    this.results = params.results
+    this.onRequestItem = params.onRequestItem;
+    this.onRequestIn = params.onRequestIn;
+    this.onRequestOut = params.onRequestOut;
+    this.results = params.results;
+    this.fetching = params.fetching;
 
-    this.handleItemClick = this.handleItemClick.bind(this)
+    this.handleItemClick = this.handleItemClick.bind(this);
+    this.handleMouseIn = this.handleMouseIn.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
   }
 
   handleItemClick(data, event){
     this.onRequestItem(data)
+  }
+
+  handleMouseIn(data, event){
+    this.onRequestIn(data);
+  }
+
+  handleMouseOut(data, event){
+    this.onRequestOut(data);
   }
 }
