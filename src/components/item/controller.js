8912aed27a6e408654937110af4d6a383b0d6683
DavidMBP <steadypathapp@gmail.com>
2017-04-25 20:41:09 -0500

    Added ItemViewModel docs

--- src/components/item/controller.js
+++ src/components/item/controller.js
@@ -1,36 +1,52 @@
-import ko from 'knockout';
 import { component } from 'utils/decorators';
-
-
-import { fakeResult } from '../../../fake.js';
-
 
 
 @component({
   name: 'item',
   template: require('./view.html')
 })
-class ItemController {
+/** KnockoutJS ViewModel that represents the Item Panel */
+class ItemViewModel {
+
+  /**
+  * @constructor ItemViewModel
+  * @param {object} place - The place details to display
+  * @param {boolean} show - Whether to show the panel or not
+  * @param {function} onRequestBack - The callback called when the back link
+  * @member {boolean} over - whether user has scrolled over the cover image
+  */
   constructor(params) {
     this.place = params.place;
     this.show = params.show;
-    this.over = false
+    this.over = false;
 
-    this.onRequestBack = params.onRequestBack
+    this.onRequestBack = params.onRequestBack;
   }
 
+  /**
+  * Scroll Event handler for the item panel. Changes the background color
+  * of search bar
+  * @param {object} data
+  * @param {object} event - scroll event
+  */
   handleScroll(data, event) {
-    if(event.target.scrollTop > event.target.clientHeight * 0.3 && !this.over){
-      document.getElementById('item-search-background').style.opacity = "1"
-      this.over = true
+    if (event.target.scrollTop > event.target.clientHeight * 0.3 && !this.over) {
+      document.getElementById('item-search-background').style.opacity = "1";
+      this.over = true;
     }
-    if(event.target.scrollTop < event.target.clientHeight * 0.3 && this.over) {
-      document.getElementById('item-search-background').style.opacity = "0"
-      this.over = false
+    if (event.target.scrollTop < event.target.clientHeight * 0.3 && this.over) {
+      document.getElementById('item-search-background').style.opacity = "0";
+      this.over = false;
     }
   }
 
+  /**
+  * Click Event handler for when the return to results link is pressed
+  * @param {object} data
+  * @param {object} event - click event
+  */
   handleBack(data, event) {
-    this.onRequestBack()
+    event.preventDefault();
+    this.onRequestBack();
   }
 }
