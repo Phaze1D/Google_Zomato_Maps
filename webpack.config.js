cc54263ebccfd4fcf9692d8e01d1ae19a01d6887
DavidMBP <steadypathapp@gmail.com>
2017-04-26 12:39:50 -0500

    Added production scrip

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 
 var config = {
@@ -45,7 +47,18 @@ var config = {
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'src'),
-  }
+  },
+  plugins: []
+}
+
+if(process.env.NODE_ENV === 'production'){
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({ output: { comments: false } }))
+  config.plugins.push(new HtmlWebpackPlugin({
+      inject: 'head',
+      filename: 'index.html',
+      template: 'src/index.html'
+    }))
+  config.watch = false
 }
 
 module.exports = config
